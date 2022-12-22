77e3cd6d179e41352a4a2dc1850d2117b561afc4
Steven Tripari <07-bob.warble@icloud.com>
2022-12-22 17:14:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+
+Steven Tripari - Creative Problem Solver!
+
+Design Research Specialist at Macy's | The Webby Awards 2022 Official Honoree - 95th Macy's Thanksgiving Day Parade NFTs | Building Web3.0 | User Experience Evangelist | n00b Developer
+
+🐦 Twitter:    https://twitter.com/steventripari
+📖 Github:     https://github.com/steventripari 
+💼 Linkedin:   https://www.linkedin.com/in/stripari/ 
+
+`);
