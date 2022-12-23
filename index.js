fbdd5652302625359189782b2402a31e0e63ee89
Steven Tripari <07-bob.warble@icloud.com>
2022-12-23 14:47:10 -0500

    👌 IMPROVE: Colors

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk'); //chalk 5 in ESM, using chalk 4.0
+const log = console.log;
+const dim = chalk.dim;
+const italic = chalk.italic;
+const twitterClr = chalk.hex(`#1da1f2`).bold.inverse
+const githubClr = chalk.hex(`#6cc644`).bold.inverse
+const linkedinClr = chalk.hex(`#0077b5`).bold.inverse
 
 welcome({
-	title: pkgJSON.name,
+	title: `Steven Tripari`,
 	tagLine: `gm! We're all gonna make it.'`,
 	description: pkgJSON.description,
     bgColor: `#FADC00`,
@@ -13,13 +20,12 @@ welcome({
 	version: pkgJSON.version,
 });
 
-console.log(`
-Steven Tripari - Creative Problem Solver!
+log(`${italic(
+    `Design Research Specialist at Macy's | The Webby Awards 2022 Official Honoree - 95th Macy's Thanksgiving Day Parade NFTs | Building Web3.0 | User Experience Evangelist | n00b Developer`
+)}
 
-Design Research Specialist at Macy's | The Webby Awards 2022 Official Honoree - 95th Macy's Thanksgiving Day Parade NFTs | Building Web3.0 | User Experience Evangelist | n00b Developer
-
-🐦 Twitter:    https://twitter.com/steventripari
-📖 Github:     https://github.com/steventripari 
-💼 Linkedin:   https://www.linkedin.com/in/stripari/ 
-
+🐦 ${twitterClr(` Twitter `)}:    ${dim(`https://twitter.com/steventripari`)}
+📖 ${githubClr(` GitHub `)}:     ${dim(`https://github.com/steventripari`)} 
+💼 ${linkedinClr(` LinkedIn `)}:   ${dim(`https://www.linkedin.com/in/stripari/`)} 
 `);
+
